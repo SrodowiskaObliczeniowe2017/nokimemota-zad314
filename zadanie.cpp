c7d5a23ca332d75678d186a7feacdef577e8d86c
student <student@lab404-03.lab.metal.agh.edu.pl>
2017-10-18 16:06:38 +0200

    wprowadzanie danych

--- zadanie.cpp
+++ zadanie.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main(){
-	float d[s];
-	for(int i=0;i<s;i++){
+	int n, k;
+	cout<<"Podaj wartosc n: ";
+	cin>>n;
+	cout<<"Podaj wartosc k: ";
+	cin>>k;
+	double* a = new double [n];
+	cout<<"Podaj wartosci kandydatow: ";
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	double* d = new double [n];
+	for(int i=0;i<n;i++){
 		for(int j=1;j<i;j++)
-			d[i]=(a[j]*(j-1)-(n-i)*a[i])
+			d[i]=(a[j]*(j-1)-(n-i)*a[i]);
 	}
 	return 0;
+}
